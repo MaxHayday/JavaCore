89f91db8249dfafd92170c25587acdf7cfc3d7e3
MaxHayday <filipenko.91@mail.ru>
2020-08-06 00:25:30 +0300

    Example2

---
+++ src/main/java/com/max_hayday/javacore/chapter02/Example2.java
@@ -0,0 +1,13 @@
+package com.max_hayday.javacore.chapter02;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num = 100;
+
+        System.out.println("It`s variable num: " + num);
+
+        num *= 2;
+        System.out.println("Value variable num * 2 is ");
+        System.out.println(num);
+    }
+}
