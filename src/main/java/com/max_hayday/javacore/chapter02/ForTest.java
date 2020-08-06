391b64f75a6f32524f1b0a230d1cdc98b0ef48fb
MaxHayday <filipenko.91@mail.ru>
2020-08-06 20:36:07 +0300

    ForTest

---
+++ src/main/java/com/max_hayday/javacore/chapter02/ForTest.java
@@ -0,0 +1,9 @@
+package com.max_hayday.javacore.chapter02;
+
+public class ForTest {
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x+=1) {
+            System.out.println("Value x: " + x);
+        }
+    }
+}
