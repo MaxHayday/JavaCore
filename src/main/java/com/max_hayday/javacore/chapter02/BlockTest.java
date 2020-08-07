5a24e6f93853b73fd26827d0c10f8f89490b074d
MaxHayday <filipenko.91@mail.ru>
2020-08-07 20:30:01 +0300

    BlockTest

--- src/main/java/com/max_hayday/javacore/chapter02/BlockTest.java
+++ src/main/java/com/max_hayday/javacore/chapter02/BlockTest.java
@@ -1,7 +1,7 @@
 package com.max_hayday.javacore.chapter02;
 
 public class BlockTest {
-    
+
     public static void main(String[] args) {
         int x, y = 20;
         for (x = 0; x < 10; x++) {
