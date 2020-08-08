585603464cba1525ce3f468e52c5a660270ddeeb
MaxHayday <filipenko.91@mail.ru>
2020-08-08 23:13:45 +0300

    Calculate the area of a circle

---
+++ src/main/java/com/max_hayday/javacore/chapter02/Area.java
@@ -0,0 +1,10 @@
+package com.max_hayday.javacore.chapter02;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi = 3.1416,//circle radius
+                r = 10.8,//approximate value of pi
+                a = pi * r * r;//calculate the area of a circle
+        System.out.println("The area of a circle is " + a);
+    }
+}
