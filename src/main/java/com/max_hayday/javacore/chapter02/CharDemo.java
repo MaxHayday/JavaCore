9af43e0d5a86fe374bc78461be1a92bdfd33292f
MaxHayday <filipenko.91@mail.ru>
2020-08-08 23:45:22 +0300

    Demonstrate the application of the char data type

---
+++ src/main/java/com/max_hayday/javacore/chapter02/CharDemo.java
@@ -0,0 +1,10 @@
+package com.max_hayday.javacore.chapter02;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1 = 88,//code of symbol X
+                ch2 = 'Y';
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
