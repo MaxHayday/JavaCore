5fe65fe5a1c4fa3249b111bff3510b95fb860dbd
MaxHayday <filipenko.91@mail.ru>
2020-08-09 01:49:20 +0300

    Code block scope

---
+++ src/main/java/com/max_hayday/javacore/chapter02/Scope.java
@@ -0,0 +1,14 @@
+package com.max_hayday.javacore.chapter02;
+
+public class Scope {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x == 10) {
+            int y = 20;
+            System.out.println("x and y: " + x + " " + y);
+            x = y * 2;
+        }
+        //y = 100; //Error! Variable y isn`t available in this scope, but x is available.
+        System.out.println("x equals " + x);
+    }
+}
