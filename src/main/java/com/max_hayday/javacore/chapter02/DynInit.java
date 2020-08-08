1197c059d17c5fdcfe64ac5708b52b2f957ee990
MaxHayday <filipenko.91@mail.ru>
2020-08-09 01:29:44 +0300

    This example demonstrates dynamic initialization

---
+++ src/main/java/com/max_hayday/javacore/chapter02/DynInit.java
@@ -0,0 +1,11 @@
+package com.max_hayday.javacore.chapter02;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        //dynamic variable initialization c
+        double c = Math.sqrt(a * a + b * b);
+        System.out.println("The hypothesis is " + c);
+    }
+}
