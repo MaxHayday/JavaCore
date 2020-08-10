d88657bc768e0b2fc377c46e4c38fecea825d248
MaxHayday <filipenko.91@mail.ru>
2020-08-10 23:30:53 +0300

    Simple example using operator switch

---
+++ src/main/java/com/max_hayday/javacore/chapter05/SampleSwitch.java
@@ -0,0 +1,24 @@
+package com.max_hayday.javacore.chapter05;
+
+public class SampleSwitch {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++)
+            switch (i) {
+                case 0:
+                    System.out.println("i equals 0.");
+                    break;
+                case 1:
+                    System.out.println("i equals 1.");
+                    break;
+                case 2:
+                    System.out.println("i equals 2.");
+                    break;
+                case 3:
+                    System.out.println("i equals 3.");
+                    break;
+                default:
+                    System.out.println("i more than 3.");
+            }
+
+    }
+}
