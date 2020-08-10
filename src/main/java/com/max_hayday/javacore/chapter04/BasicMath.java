2a15901c60f65ffb49f22e7a32c840865505e31c
MaxHayday <filipenko.91@mail.ru>
2020-08-10 09:53:44 +0300

    Demonstrate basic arithmetic operations

---
+++ src/main/java/com/max_hayday/javacore/chapter04/BasicMath.java
@@ -0,0 +1,31 @@
+package com.max_hayday.javacore.chapter04;
+
+public class BasicMath {
+    public static void main(String[] args) {
+        // arithmetic operations on integer values
+        System.out.println("Integer arithmetics");
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+        System.out.println("a = " + a);
+        System.out.println("a = " + b);
+        System.out.println("a = " + c);
+        System.out.println("a = " + d);
+        System.out.println("a = " + e);
+
+        // arithmetic operations on double values
+        System.out.println("\nDouble arithmetics");
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = db / 4;
+        double dd = dc - a;
+        double de = -dd;
+        System.out.println("da = " + da);
+        System.out.println("db = " + db);
+        System.out.println("dc = " + dc);
+        System.out.println("dd = " + dd);
+        System.out.println("de = " + de);
+    }
+}
