3797276b7f6293b2bb0fd5186277774cf178fb39
MaxHayday <filipenko.91@mail.ru>
2020-08-11 15:58:54 +0300

    Use String for operator control in switch

---
+++ src/main/java/com/max_hayday/javacore/chapter05/SpringSwitch.java
@@ -0,0 +1,20 @@
+package com.max_hayday.javacore.chapter05;
+
+public class SpringSwitch {
+    public static void main(String[] args) {
+        String str = "two";
+        switch (str) {
+            case "one":
+                System.out.println("one");
+                break;
+            case "two":
+                System.out.println("two");
+                break;
+            case "three":
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("did`t match");
+        }
+    }
+}
