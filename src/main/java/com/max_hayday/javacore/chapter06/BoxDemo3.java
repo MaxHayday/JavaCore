f8226eb02700e67d2ea45237818704f36f4c1098
MaxHayday <filipenko.91@mail.ru>
2020-08-12 00:20:38 +0300

    This program applies the method introduced in the Box class

---
+++ src/main/java/com/max_hayday/javacore/chapter06/BoxDemo3.java
@@ -0,0 +1,30 @@
+package com.max_hayday.javacore.chapter06;
+
+public class BoxDemo3 {
+    public static void main(String[] args) {
+        Box myBox1 = new Box();
+        Box myBox2 = new Box();
+
+        myBox1.width = 10;
+        myBox1.height = 20;
+        myBox1.depth = 15;
+
+        myBox2.width = 3;
+        myBox2.height = 6;
+        myBox2.depth = 9;
+
+        myBox1.volume();
+        myBox2.volume();
+    }
+
+    static class Box {
+        double width;
+        double height;
+        double depth;
+
+        void volume() {
+            System.out.print("Volume is ");
+            System.out.println(width * height * depth);
+        }
+    }
+}
