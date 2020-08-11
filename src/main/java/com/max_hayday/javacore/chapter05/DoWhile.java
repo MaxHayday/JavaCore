2f0b13bb5edcda26779adfbb44bc8715c426e5f5
MaxHayday <filipenko.91@mail.ru>
2020-08-11 16:24:19 +0300

    Demonstrate the use operator loop do-while

---
+++ src/main/java/com/max_hayday/javacore/chapter05/DoWhile.java
@@ -0,0 +1,11 @@
+package com.max_hayday.javacore.chapter05;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do {
+            System.out.println("tact " + n);
+            n--;
+        } while (n > 0);
+    }
+}
