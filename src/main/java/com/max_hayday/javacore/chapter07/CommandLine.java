1eadcab4ee2ec656334ea46368127e54d1588dbf
MaxHayday <filipenko.91@mail.ru>
2020-08-13 00:49:34 +0300

    List all command line arguments

---
+++ src/main/java/com/max_hayday/javacore/chapter07/CommandLine.java
@@ -0,0 +1,9 @@
+package com.max_hayday.javacore.chapter07;
+
+public class CommandLine {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]: " + args[i]);
+        }
+    }
+}
