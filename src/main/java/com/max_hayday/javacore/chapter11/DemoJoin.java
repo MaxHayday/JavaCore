27c5a9aa104431af56b1ac123757ff31bf80f369
MaxHayday <filipenko.91@mail.ru>
2020-10-25 15:51:05 +0200

    Using method join() for waiting end of threads

--- src/main/java/com/max_hayday/javacore/chapter11/DemoJoin.java
+++ src/main/java/com/max_hayday/javacore/chapter11/DemoJoin.java
@@ -1,15 +1,20 @@
 package com.max_hayday.javacore.chapter11;
 
-
-public class MultiThreadDemo {
+public class DemoJoin {
     public static void main(String[] args) {
-        new NewThread("One");//run threads for execution
-        new NewThread("Two");
-        new NewThread("Three");
+        NewThread ob1 = new NewThread("One");
+        NewThread ob2 = new NewThread("Two");
+        NewThread ob3 = new NewThread("Three");
+        System.out.println("Thread ONE is run: " + ob1.t.isAlive());
+        System.out.println("Thread TWO is run: " + ob2.t.isAlive());
+        System.out.println("Thread THREE is run: " + ob3.t.isAlive());
 
+
+        //waiting for finished threads for execution
         try {
-            //waiting for finished threads for execution
-            Thread.sleep(10000);
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
         } catch (InterruptedException e) {
             System.out.println("Main thread is interrupted.");
         }
