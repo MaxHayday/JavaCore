e4d0c04363d2f22c8f022cd31c68725fcaec4dd8
MaxHayday <filipenko.91@mail.ru>
2020-10-26 13:19:10 +0200

    Incorrect implementation of producer and consumer

---
+++ src/main/java/com/max_hayday/javacore/chapter11/PC.java
@@ -0,0 +1,60 @@
+package com.max_hayday.javacore.chapter11;
+
+
+public class PC {
+    public static void main(String[] args) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+
+        System.out.println("For stop push Ctrl-C.");
+    }
+
+
+    static class Q {
+        int n;
+
+        synchronized int get() {
+            System.out.println("Received: " + n);
+            return n;
+        }
+
+        synchronized void put(int n) {
+            this.n = n;
+            System.out.println("Sent: " + n);
+        }
+    }
+
+    static class Producer implements Runnable {
+        Q q;
+
+        public Producer(Q q) {
+            this.q = q;
+            new Thread(this, "producer").start();
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+            while (true) {
+                q.put(i++);
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+        Q q;
+
+        Consumer(Q q) {
+            this.q = q;
+            new Thread(this, "Consumer").start();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                q.get();
+            }
+        }
+    }
+}
