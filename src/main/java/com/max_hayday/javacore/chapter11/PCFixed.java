d2807cac5d4c31993498938f1971e92c261aff1d
MaxHayday <filipenko.91@mail.ru>
2020-10-27 13:57:34 +0200

    Correct realization producer and consumer

---
+++ src/main/java/com/max_hayday/javacore/chapter11/PCFixed.java
@@ -0,0 +1,76 @@
+package com.max_hayday.javacore.chapter11;
+
+public class PCFixed {
+    public static void main(String[] args) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+
+        System.out.println("For stop push Ctrl-C.");
+    }
+
+    static class Q {
+        int n;
+        boolean valueSet = false;
+
+        synchronized int get() {
+            while (!valueSet)
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted Exception is catch.");
+                }
+            System.out.println("Received: " + n);
+            valueSet = false;
+            notify();
+            return n;
+        }
+
+        synchronized void put(int n) {
+            while (valueSet) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted Exception is catch.");
+                }
+                this.n = n;
+                valueSet = true;
+                System.out.println("Sent: " + n);
+                notify();
+            }
+        }
+    }
+
+    static class Producer implements Runnable {
+        Q q;
+
+        public Producer(Q q) {
+            this.q = q;
+            new Thread(this, "producer").start();
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+            while (true) {
+                q.put(i++);
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+        Q q;
+
+        Consumer(Q q) {
+            this.q = q;
+            new Thread(this, "Consumer").start();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                q.get();
+            }
+        }
+    }
+}
